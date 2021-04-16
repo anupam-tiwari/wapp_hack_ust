d94555be259759174ae3bf78c385d597f1671b4c
unknown <appleanupam@gmail.com>
2021-04-16 18:09:53 +0530

    first commit

---
+++ includes/connection.php
@@ -0,0 +1,18 @@
+<?php
+//$con = mysqli_connect("localhost","root","","social_network");
+$con = mysqli_connect("sql6.freesqldatabase.com" ,"sql6406017" ,"kgCjSY6Kpu" ,"sql6406017");
+if(!$con){
+    echo "Not connected";
+}else{
+    echo "connected";
+
+}
+ //   $server = "sql303.epizy.com";
+//    $username = "epiz_28405349";
+//    $password = "ibwQOhtQsoW5V";
+//    $dbname = "epiz_28405349_wap";
+
+//$con = mysqli_connect($server, $username, $password, $dbname );
+?>
+
+
